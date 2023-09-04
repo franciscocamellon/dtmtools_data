58ea3a37c32e329d59b1ff82a14474629dc5500d
franciscocamellon <franciscocamellon@gmail.com>
2023-09-03 23:34:44 -0300

    end of the day commit

--- Untitled-3.py
+++ Untitled-3.py
@@ -2,19 +2,19 @@ cocuruto = QgsProject.instance().mapLayersByName('Cocuruto')[0]
 peaks = QgsProject.instance().mapLayersByName('peak_points')[0]
 
 polylines = [polyline.geometry().asPolyline() for polyline in cocuruto.getFeatures()]
-
+print(polylines)
 contains = {}
 
 for index, contour in enumerate(polylines):
     closed_contour = QgsGeometry.fromPolygonXY([contour])
     print(closed_contour)
-    for spot in peaks.getFeatures():
-        point_geom = spot.geometry()
-        print(point_geom)
-        
-        if closed_contour.contains(point_geom):
-            print('contains')
-            break
+#    for spot in peaks.getFeatures():
+#        point_geom = spot.geometry()
+#        print(point_geom)
+#        
+#        if closed_contour.contains(point_geom):
+#            print('contains')
+#            break
 #            contains[index] = point_geom
             
 #print(contains)
